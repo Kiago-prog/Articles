84d87c01db9d9333208d8eb371a822bdd34bc62b
kiago <roy.kiago@student.moringaschool.com>
2025-05-26 10:18:29 +0300

    Test

--- code-challenge/tests/test_magazine.py
+++ code-challenge/tests/test_magazine.py
@@ -1,9 +1,30 @@
+import pytest
 from lib.models.magazine import Magazine
+from lib.models.author import Author
+from lib.models.article import Article
+from lib.db.connection import get_connection
 
-def test_magazine_can_be_saved_and_retrieved():
-    magazine = Magazine(name="Nature Monthly", category="Science")
-    magazine.save()
+def setup_function():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.executescript("""
+        DELETE FROM articles;
+        DELETE FROM magazines;
+        DELETE FROM authors;
+    """)
+    conn.commit()
 
-    fetched = Magazine.find_by_id(magazine.id)
-    assert fetched.name == "Nature Monthly"
-    assert fetched.category == "Science"
+def test_create_magazine():
+    mag = Magazine.create("Tech Today", "Technology")
+    assert mag.name == "Tech Today"
+    assert mag.category == "Technology"
+
+def test_contributors_and_titles():
+    mag = Magazine.create("Science World", "Science")
+    author = Author.create("Einstein")
+    Article.create("Relativity", author.id, mag.id)
+    titles = mag.article_titles()
+    contributors = mag.contributors()
+    
+    assert titles == ["Relativity"]
+    assert len(contributors) == 1
